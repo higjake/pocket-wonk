7e8716b9254c222590bd9ccef7e902452a35e925
higjake <higjake@gmail.com>
2017-04-14 12:55:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,11 @@ def makeQuery(req):
     state = parameters.get("state")
     target_metric = parameters.get("target-metric")
     year = parameters.get("year") or "2016"
+    county = parameter.get("county") or "null"
+    if county = "null":
+        return year + "/pep/population?get=GEONAME," + target_metric + "&for=state:" + state
     
-    return year + "/pep/population?get=GEONAME," + target_metric + "&for=state:" + state
+    return year + "/pep/population?get=GEONAME," + target_metric + "&for=county:" + county + "&in=county:" + state
 
 def makeWebhookResult(data):
     array1 = data[1]
