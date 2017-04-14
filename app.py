76e7cab7184690714c93039df809cd37cd4f9762
higjake <higjake@gmail.com>
2017-04-14 00:46:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def makeQuery(req):
     return state
 
 def makeWebhookResult(data, action):
-    firstresult = data[0]
-    if firstresult is None:
+    array = data[0]
+    if array is None:
         return {
         "speech": "this failed",
         "displayText": "this failed",
@@ -70,8 +70,8 @@ def makeWebhookResult(data, action):
         "source": "apiai-weather-webhook-sample"}
     
     # print(json.dumps(item, indent=4))
-    firstresult = data[0] # Adding this line as a sanity check
-    speech = firstresult[0]
+    array = data[0] # Adding this line as a sanity check
+    speech = array[0][0]
 #     actionMap[action]['speech'] % tuple([providers[i].get(actionMap[action]['key']) for i in range(actionMap[action]['count'])]);
 #     speech = "The top three providers in your area are " + providers[0].get('business_name') + ", " + providers[1].get('business_name') + ", and " + providers[2].get('business_name') + "." 
     print("Response:")
