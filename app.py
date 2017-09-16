c1b1cc6876e49b04f7184846addc9d5a94e6f9d2
higjake <higjake@gmail.com>
2017-09-16 13:55:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,24 @@
-#!/usr/bin/env python
-from __future__ import print_function
-from future.standard_library import install_aliases
-install_aliases()
-from urllib.parse import urlparse, urlencode
-from urllib.request import urlopen, Request
-from urllib.error import HTTPError
-import json
-import os
-import time
-from flask import Flask
-from flask import request
-from flask import make_response
+# Replace with the correct URL
+url = "https://api.propublica.org/congress/v1/115/senate/members.json"
+headers = {
+    'X-API-KEY': 'kuJPmTSeZ8Qt7dXn1yIzJbXCkSHI6E5FXNB4tRMM'
+}
+# It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
+myResponse = requests.get(url,headers=headers)
+print (myResponse.status_code)
 
-videoArray = ['https://www.youtube.com/watch?v=9ogQ0uge06o','https://www.youtube.com/watch?v=V-zXT5bIBM0','https://www.youtube.com/watch?v=nXpB1rixnPQ','https://www.youtube.com/watch?v=Pa0lMzaljTk','https://www.youtube.com/watch?v=nbY_aP-alkw','https://www.youtube.com/watch?v=nPImqZo0D74']
+# For successful API call, response code will be 200 (OK)
+if(myResponse.ok):
 
-# Flask app should start in global layout
-app = Flask(__name__)
-@app.route('/webhook', methods=['POST'])
-def webhook():
-    start = time.time()
-    req = request.get_json(silent=True, force=True)
-    print("Request:")
-    print(json.dumps(req, indent=4))
-    res = processRequest(req)
-    res = json.dumps(res, indent=4)
-    # print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    end = time.time()
-    print('Duration: {:10.4f} seconds'.format(end - start))
-    return r
-def processRequest(req):
-    if req.get('result').get('action') != 'randomdisney':
-        return {}
-    import random
-    random_number = random.randint(0,5)
-    print(random_number)
-    speech = videoArray[random_number]
-    print("Response:")
-    print(speech)
-    slack_text = "<" + speech + ">"
-    print("Slack Response:")
-    print(slack_text)
-    slack_message = {
-        "text": slack_text,
-    }
-    return {
-        "speech": speech,
-        "displayText": speech,
-        "data": {
-            "slack": slack_message
-        },
-        # "contextOut": [],
-        "source": "Jake Higdon"
-    }
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    print("Starting app on port %d" % port)
-    app.run(debug=False, port=port, host='0.0.0.0')
+	# Loading the response data into a dict variable
+	# json.loads takes in only binary or string variables so using content to fetch binary content
+	# Loads (Load String) takes a Json file and converts into python data structure (dict or list, depending on JSON)
+	jData = json.loads(myResponse.content)
+	
+	print("The response contains {0} properties".format(len(jData)))
+	print("\n")
+	for key in jData:
+		print key + " : " + jData[key]
+else:
+  # If response code is not ok (200), print the resulting http error code with description
+	myResponse.raise_for_status()
